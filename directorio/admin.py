320d2c39c14003a63e6115b7e00ed34e901ed01a
unknown <hapauki@gmail.com>
2023-07-07 12:03:38 -0500

    resolver modificación de usuarios pendiente

--- directorio/admin.py
+++ directorio/admin.py
@@ -1,25 +1,38 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
-from .forms import UsuarioForm
+from .forms import UsuarioForm, UserUpdateForm
+import logging
+
+logger = logging.getLogger(__name__)
+
 # Register your models here.
-class CustomUserAdmin(BaseUserAdmin):
+class CustomUserAdmin(UserAdmin):
     add_form = UsuarioForm
     form = UsuarioForm
-
-    list_display = ('email', 'is_staff', 'is_superuser')
-    list_filter = ('is_staff', 'is_superuser')
+    model = User
+    list_display = ['email', 'is_staff']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_superuser')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_superuser'),
+            'fields': ('email', 'password1', 'password2'),
         }),
     )
-    search_fields = ('email',)
     ordering = ('email',)
+    filter_horizontal = ()
+
+    def save_model(self, request, obj, form, change):
+        try:
+            super().save_model(request, obj, form, change)
+        except Exception as e:
+            logger.exception("Error al guardar el modelo:")
+            raise e
+        
+class UserUpdateAdmin(UserAdmin):
+    form = UserUpdateForm
 
 admin.site.register(User, CustomUserAdmin)
